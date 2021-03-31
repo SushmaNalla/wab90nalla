515ffe5a937f2ecd4953fb784cac3b3f042de180
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2021-03-31 00:33:43 -0500

    class Book

--- routes/Book.js
+++ routes/Book.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results Book' });
+  res.render('book', { title: 'Search Results Book' });
 });
 
 module.exports = router;
