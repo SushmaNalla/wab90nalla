b159153033b893dc19999875346c2f9e2b97c4a7
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2021-03-31 02:03:11 -0500

    files

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # wab90nalla
 Assignment 10
 Link to heroku server https://wab90nalla.herokuapp.com/
-Class name: Book
-Attributes:Name (String),Price (Number),Author (String)
+Class name: Book Attributes:Name (String),Price (Number),Author (String)
--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bookRouter = require('./routes/book');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -23,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/book', bookRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
